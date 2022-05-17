348cb2fe38612c0fe946ad3d582b97f19740816c
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-17 10:48:41 +0530

    [Suraj]: Usecase to calculate daily wages is added.

--- EmployeeWages/EmployeeWages/Employee.cs
+++ EmployeeWages/EmployeeWages/Employee.cs
@@ -8,14 +8,21 @@ namespace EmployeeWages
 {
     internal class Employee
     {
+        int FullTime = 1;
+        int WagesPerHour = 20;
         public void getAttendance()
         {
-            int FullTime = 1;
+            
             Random random = new Random();
             int attendance = random.Next(0, 2);
 
             if (attendance == FullTime)
+            {
                 Console.WriteLine("Employee is present.");
+                int empHrs = 8;
+                double salary = empHrs * WagesPerHour;
+                Console.WriteLine("Employee salary for the day is: "+ salary);
+            }
             else
                 Console.WriteLine("Employee is absent.");
         }
