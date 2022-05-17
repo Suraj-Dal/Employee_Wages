b68f1ec5c873aaa5896d74098c331b47d423eb79
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-17 10:57:46 +0530

    [Suraj]: Usecase to calculate part time wages is added.

--- EmployeeWages/EmployeeWages/Employee.cs
+++ EmployeeWages/EmployeeWages/Employee.cs
@@ -9,12 +9,13 @@ namespace EmployeeWages
     internal class Employee
     {
         int FullTime = 1;
+        int PartTime = 2;
         int WagesPerHour = 20;
         public void getAttendance()
         {
             
             Random random = new Random();
-            int attendance = random.Next(0, 2);
+            int attendance = random.Next(0, 3);
 
             if (attendance == FullTime)
             {
@@ -22,6 +23,13 @@ namespace EmployeeWages
                 int empHrs = 8;
                 double salary = empHrs * WagesPerHour;
                 Console.WriteLine("Employee salary for the day is: "+ salary);
+            }
+            else if (attendance == PartTime)
+            {
+                Console.WriteLine("Employee present for part time.");
+                int empHrs = 4;
+                double salary = empHrs * WagesPerHour;
+                Console.WriteLine("Employee salary for part time is: "+ salary);
             }
             else
                 Console.WriteLine("Employee is absent.");
