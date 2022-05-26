f6910f674c4ef1d623a5eb70eeb9577a5c389537
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-26 17:32:54 +0530

    [Suraj]: Uascase 10 to calculate wages for multiple companies using array is added with some refactorations.

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -60,23 +60,6 @@ namespace EmployeeWages
 
 
 
-/*
-//----------------------------------
-using System;
-
-namespace Employee
-{
-    internal class Employee
-    {
-        public static void Main(String[] args)
-        {
-            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-            empWageBuilder.addCompanyEmpWage("CompanyA", 20, 30, 100);
-            empWageBuilder.addCompanyEmpWage("CompanyB", 25, 25, 90);
-            empWageBuilder.computeEmpWage();
-        }
-    }
-}*/
 
 
 
